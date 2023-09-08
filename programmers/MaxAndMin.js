6bbbf7231cbf3c084ceda2740463fe44c3518989
lydiacho <sheecho0804@ewhain.net>
2023-09-08 22:35:21 +0900

    0908 최댓값과 최솟값(정답)

--- programmers/MaxAndMin.js
+++ programmers/MaxAndMin.js
@@ -11,12 +11,22 @@
 
 // Point : Math.max()와 Math.min() 메소드의 인수는 숫자의 배열이 아니라 여려개의 숫자들이어야 한다. 따라서 인수로 배열을 넣으면 리턴값이 NaN이 되므로 스프레드연사자를 통해 배열의 형태를 풀어줘야 한다.
 
+// 1차시도
+// 틀린 이유 : 음수일 경우 숫자가 한글자가 아닌 두글자로 이루어져 게속 NaN이 나오는 문제에 봉착했었다.
+// function solution(s) {
+//   var answer = "";
+//   let array = [];
+//   for (let i = 0; i < s.length; i += 2) {
+//     array.push(parseInt(s[i]));
+//   }
+//   console.log(array);
+//   answer += Math.min(...array).toString() + " " + Math.max(...array).toString();
+//   return answer;
+// }
+
 function solution(s) {
   var answer = "";
-  let array = [];
-  for (let i = 0; i < s.length; i += 2) {
-    array.push(parseInt(s[i]));
-  }
-  answer += Math.max(...array).toString() + " " + Math.min(...array).toString();
+  let array = s.split(" ").map((el) => parseInt(el));
+  answer += Math.min(...array) + " " + Math.max(...array);
   return answer;
 }
