7747f1720cbfff607291fa1290e723600bf8e902
lydiacho <sheecho0804@ewhain.net>
2024-03-19 00:10:43 +0900

    0319 디스크 컨트롤러 90점짜리

--- programmers/디스크_컨트롤러.js
+++ programmers/디스크_컨트롤러.js
@@ -40,7 +40,8 @@ class MinHeap {
       let currentIndex = index ? index : this.size()-1; // 특정 요소 있으면 넣고 없으면 마지막 노드 
       let parentIndex = this.getParentIndex(currentIndex);
   
-      while (parentIndex >= 0 && this.heap[currentIndex][0] < this.heap[parentIndex][0]) {
+      while (parentIndex >= 0 && (this.heap[currentIndex][0] <= this.heap[parentIndex][0])) {
+          if (this.heap[currentIndex][0] == this.heap[parentIndex][0] && this.heap[currentIndex][1] >= this.heap[parentIndex][1]) break;
           this.swap(currentIndex, parentIndex);
   
           currentIndex = parentIndex;
@@ -53,7 +54,8 @@ class MinHeap {
       let rightChildIndex = 2;
       let minChildIndex = (this.heap[rightChildIndex] && this.heap[rightChildIndex][0] < this.heap[leftChildIndex][0] ? rightChildIndex : leftChildIndex);
 
-      while (this.heap[minChildIndex] && this.heap[currentIndex][0] > this.heap[minChildIndex][0]) {
+      while (this.heap[minChildIndex] && this.heap[currentIndex][0] >= this.heap[minChildIndex][0]) {
+        if (this.heap[currentIndex][0] == this.heap[minChildIndex][0] && this.heap[currentIndex][1] <= this.heap[minChildIndex][1]) break;
           this.swap(currentIndex, minChildIndex);
   
           currentIndex = minChildIndex;
@@ -66,7 +68,7 @@ class MinHeap {
 
 function solution(jobs) {
   const answer = [];
-  const heap = new MinHeap(0);
+  const heap = new MinHeap();
   jobs.forEach((v)=>heap.heapPush(v));
   let updateTime = heap.heap[0][0];
 
@@ -74,9 +76,18 @@ function solution(jobs) {
   updateTime += task[1];
   answer.push(updateTime-task[0]);
 
-  while(!heap.isEmpty()){
+  while(true){
     const tempArr = heap.heap.filter((v)=> v[0]<=updateTime );
-    tempArr.sort((a,b)=>a[1]-b[1]);
+    if (tempArr.length===0) {
+      // 작업 스케줄에 텀 발생 
+      console.log("텀 발생 : ",heap.heap[0])
+      const task = heap.heapPop();
+      updateTime += task[1];
+      answer.push(updateTime-task[0]);
+      continue;
+    }
+
+    tempArr.sort((a,b)=>a[1]-b[1])
     const removeIdx = heap.heap.indexOf(tempArr[0]);
 
     const tempRoot = heap.heap[0];
@@ -90,8 +101,11 @@ function solution(jobs) {
     
 
   }
-
-  answer.push(updateTime+heap.heap[0][1]-heap.heap[0][0]);
+  if (heap.heap[0][0] > updateTime) {
+    answer.push(heap.heap[0][1]);
+  } else {
+      answer.push(updateTime+heap.heap[0][1]-heap.heap[0][0]);
+  }
 
 
   let avg = 0;
@@ -99,4 +113,6 @@ function solution(jobs) {
   return Math.floor(avg/jobs.length);
 }
 
-// 8, 19 실패. 19, 20 런타임에러 => 80/100점
+// 19 실패, 20 런타임에러 => 90/100 점 
+console.log(solution([[0, 5], [2, 10], [10000, 2]]))
+
