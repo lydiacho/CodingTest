c795f52d7e001e4cb18cd12296763020985b276f
lydiacho <sheecho0804@ewhain.net>
2024-03-19 20:08:25 +0900

    0319 가장 큰 수 developed

--- programmers/가장_큰_수.js
+++ programmers/가장_큰_수.js
@@ -1,10 +1,4 @@
-const compareFn = (a,b) => {
-  if (`${a}`.length === `${b}`.length) {  // 둘의 자릿수가 같을 때
-    return b-a;
-  } 
-  return +(`${b}${a}`)-(`${a}${b}`)
-}
 const solution = (numbers) => {
-  numbers.sort(compareFn);
-  return numbers[0]===0 ? '0' : numbers.reduce((acc,cur,idx)=>acc+=cur, "");
+  const answer = numbers.map(v=>`${v}`).sort((a,b)=>+(b+a)-(a+b)).join('');
+  return answer[0]==='0' ? '0' : answer;
 }
