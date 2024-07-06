296cde701f55bfd347915e3e20e3c513c3dab576
lydiacho <sheecho0804@ewhain.net>
2024-07-07 04:31:50 +0900

    프로그래머스 > DFS/BFS > 타겟넘버

---
+++ programmers/타겟넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+  var answer = 0;
+  let idx = 0;
+  function dfs(sum, idx) {
+    if (idx === numbers.length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
+    dfs(sum - numbers[idx], idx + 1);
+    dfs(sum + numbers[idx], idx + 1);
+  }
+  dfs(0, 0);
+
+  return answer;
+}
