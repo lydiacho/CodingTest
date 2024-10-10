ac54d2ac31cbc711cb5f2a3105e49333ed2cf9c1
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-10-10 16:08:22 +0900

    [Silver I] Title: RGB거리, Time: 96 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.js
+++ 백준/Silver/1149. RGB거리/RGB거리.js
@@ -1,40 +1,24 @@
-// 1번~N번 집 빨초파로 칠하기
-// 인접한 집과 색 달라야하고, 최소 비용으로 칠해야 함
-// input : 집 수 n(2~1000), 빨초파로 칠하는 비용
-// output : 드는 최소 비용
-
-// dp(x) : 1~x번까지 집 칠하는 최소 비용
-// dp(x) = for(i) if (i!==prev) min(x번집 i색으로 칠하는 경우) ,
-// x번집 i색으로 칠하기 : dp(x+1,i)+arr[x][i]
-// 🧨관건 : 메모이제이션을 2차원배열로 관리하기.
+// 이웃한 집과 색이 같으면 안됨
+// 최소한의 비용을 사용해야 함
+// 🧨 : 이차원 배열 메모리 관리 방식을 기억하자!
 
 const fs = require("fs");
-const [N, ...input] = fs
-  .readFileSync(0)
-  .toString()
-  .trim()
-  .split("\n");
+const [N, ...rest] = fs.readFileSync(0).toString().trim().split("\n");
+
 const n = +N;
-const arr = input.map((v) => v.split(" ").map(Number));
-const MAX = 1000001;
-const mem = Array.from({ length: 10001 }, () =>
-  Array.from({ length: 4 }, () => -1)
-);
+const costs = rest.map((v) => v.split(" ").map(Number));
+const mem = Array.from({ length: n }, (_, i) => [...costs[i]]);
 
-const dp = (x, prev) => {
-  if (x === n) return (mem[x][prev] = 0);
-  if (mem[x][prev] !== -1) return mem[x][prev];
-
-  let temp = MAX;
-  for (let i = 0; i < 3; i++) {
-    if (i === prev) continue;
-    temp = Math.min(temp, dp(x + 1, i) + arr[x][i]); //x번 집에 i색을 칠하는 경우
-  }
-  mem[x][prev] = temp;
-  return mem[x][prev];
-};
 const solution = () => {
-  return dp(0, 3); // N번 집의 인덱스는 N-1
+  for (let i = 1; i < n; i++) {
+    for (let c = 0; c < 3; c++) {
+      mem[i][c] =
+        Math.min(mem[i - 1][(c - 1 + 3) % 3], mem[i - 1][(c - 2 + 3) % 3]) +
+        costs[i][c];
+    }
+  }
+
+  return Math.min(...mem[n - 1]);
 };
 
 console.log(solution());
