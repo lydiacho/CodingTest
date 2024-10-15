e45be5840027242e35794c5e3fe034b31bbdb623
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-10-15 15:54:41 +0900

    [Gold V] Title: 다각형의 면적, Time: 164 ms, Memory: 13780 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.js
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.js
@@ -1,6 +1,5 @@
 // N각형의 면적 구하기
 // output : 소수점 아래 둘째자리에서 반올림, 첫째자리까지 출력
-// 오목 / 볼록한 도형 두가지 종류 있음.
 // 면적 구하는 공식 : 1/2*|x1y2+x2y3+...+xny1 - x2y1-x3y2-...-x1yn|
 // 🧨소수점 자리 설정 : 숫자.toFixed(자릿수)
 
@@ -19,8 +18,7 @@ const solution = () => {
     sum += arr[i][0] * arr[i + 1][1];
     sum -= arr[i + 1][0] * arr[i][1];
   }
-  sum = Math.abs(sum) / 2;
-  return (Math.round(sum * 10) / 10).toFixed(1);
+  return (Math.abs(sum) / 2).toFixed(1);
 };
 
 console.log(solution());
