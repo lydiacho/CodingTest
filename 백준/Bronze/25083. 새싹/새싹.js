36544e2ad2fbcabf5876ebf52f456ab2e18feb4a
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-10-22 23:55:59 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 9280 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,12 @@
+const solution = () => {
+  const answer= [
+    `         ,r'"7`,
+    `r\`-_   ,'  ,/`,
+    ` \\. ". L_r'`,
+    `   \`~\\/`,
+    '      |',
+    '      |'
+  ];
+  return answer.join('\n');
+}
+console.log(solution());
