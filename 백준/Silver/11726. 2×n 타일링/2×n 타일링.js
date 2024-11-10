77e0d929f924373393144ec5c93b5dc44f1d2137
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-11-10 15:16:38 +0900

    [Silver III] Title: 2×n 타일링, Time: 156 ms, Memory: 9656 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,18 @@
+// 2xn 크기 직사각형을 2x1, 1x2 타일로 채우는 방법 수 구하기
+const fs = require("fs");
+const N = +fs.readFileSync(0).toString().trim();
+
+const solution = () => {
+  // dp(1) -> 1가지
+  // dp(2) -> 2가지
+  // dp(i) -> dp(i-2) + dp(i-1)
+  const mem = Array.from({ length: N + 1 }, () => 0);
+  mem[1] = 1;
+  mem[2] = 2;
+  for (let i = 3; i <= N; i++) {
+    mem[i] = ((mem[i - 2] % 10007) + (mem[i - 1] % 10007)) % 10007;
+  }
+  return mem[N];
+};
+
+console.log(solution());
