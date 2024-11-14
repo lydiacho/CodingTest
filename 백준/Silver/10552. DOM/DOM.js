da5d04d8e69bd49899da8e41f0b8411c6f4d3239
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-11-15 00:27:42 +0900

    [Silver II] Title: DOM, Time: 320 ms, Memory: 47504 KB -BaekjoonHub

--- 백준/Silver/10552. DOM/DOM.js
+++ 백준/Silver/10552. DOM/DOM.js
@@ -8,9 +8,10 @@ const [사람수, 채널수, 현재채널] = input1.split(" ").map(Number);
 const 채널취향 = rest.map((v) => v.split(" ").map(Number)); // 나이 오름차순으로 정렬
 
 const solution = () => {
-  const 싫to좋 = Array.from({ length: 채널수 + 1 }, () => []);
+  const 싫to좋 = Array.from({ length: 채널수 + 1 }, () => 0);
   채널취향.forEach(([좋, 싫]) => {
-    싫to좋[싫].push(좋);
+    if (싫to좋[싫] !== 0) return;
+    싫to좋[싫] = 좋; // 제일 어린 사람만 저장
   });
   const visited = Array.from({ length: 채널수 + 1 }, () => false);
 
@@ -24,8 +25,8 @@ const solution = () => {
   while (q.length > head) {
     const curr = q[head++];
 
-    if (싫to좋[curr].length === 0) return count;
-    const next = 싫to좋[curr][0]; // 제일 어린 사람
+    if (싫to좋[curr] === 0) return count; // 현재 채널을 싫어하는 사람이 없을 때
+    const next = 싫to좋[curr]; // 현재 채널을 싫어하는 사람이 바꾼 채널
     if (visited[next]) break;
     q.push(next);
     visited[next] = true;
